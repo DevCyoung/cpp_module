3fec8e99b551d7fb760120ea029907e63b420edc
Youngjae Seo <yoseo@c6r3s4.42seoul.kr>
2022-09-05 17:13:49 +0900

    asd

--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -8,7 +8,7 @@ class Contact
 	private :
 		std::string first_name;
 		std::string last_name;
-		std::string nickname;
+		std::string nick_name;
 		std::string phone_numbeer;
 		std::string darkest_secret;
 	public :
@@ -16,12 +16,12 @@ class Contact
 		~Contact();
 	    std::string		get_first_name();
         std::string		get_last_name();
-        std::string		get_nicke_name();
+        std::string		get_nick_name();
         std::string		get_phone_number();
         std::string		get_darkest_secret();
 		void  			set_first_name(std::string firstname);
         void  			set_last_name(std::string lastname);
-        void  			set_nicke_name(std::string nickename);
+        void  			set_nick_name(std::string nick_name);
         void  			set_darkest_secret(std::string darkestsecret);
         void        	set_phone_number(std::string phonenumber);	
 };
